74b0c87b33222555f069cf893822673301b735be
danielgranjac@gmail.com <danielgranjac@gmail.com>
2025-11-14 08:02:45 +0000

    Fix grants

--- seguridad/grants.sql
+++ seguridad/grants.sql
@@ -1,47 +1,40 @@
 -- Databricks notebook source
-GRANT USE CATALOG ON CATALOG bronze TO "Developers"
-GRANT USE CATALOG ON CATALOG bronze TO "Project RL - Data Engineers"
-
-GRANT USE CATALOG ON CATALOG silver TO "Developers"
-GRANT USE CATALOG ON CATALOG silver TO "Project RL - Data Engineers"
-
-GRANT USE CATALOG ON CATALOG gold TO "Developers"
-GRANT USE CATALOG ON CATALOG gold TO "Project RL - Data Engineers"
+GRANT USE CATALOG ON CATALOG adventureworks TO "Developers"
+GRANT USE CATALOG ON CATALOG adventureworks TO "Project RL - Data Engineers"
 
 -- COMMAND ----------
 
-GRANT USE SCHEMA ON SCHEMA bronze.adventureworks TO "Developers"
-GRANT USE SCHEMA ON SCHEMA bronze.adventureworks TO "Project RL - Data Engineers"
-GRANT CREATE ON SCHEMA bronze.adventureworks TO "Project RL - Data Engineers"
+GRANT USE SCHEMA ON SCHEMA adventureworks.bronze TO "Developers"
+GRANT USE SCHEMA ON SCHEMA adventureworks.bronze TO "Project RL - Data Engineers"
+GRANT CREATE ON SCHEMA adventureworks.bronze TO "Project RL - Data Engineers"
 
-GRANT USE SCHEMA ON SCHEMA silver.adventureworks TO "Developers"
-GRANT USE SCHEMA ON SCHEMA silver.adventureworks TO "Project RL - Data Engineers"
-GRANT CREATE ON SCHEMA silver.adventureworks TO "Project RL - Data Engineers"
+GRANT USE SCHEMA ON SCHEMA adventureworks.silver TO "Developers"
+GRANT USE SCHEMA ON SCHEMA adventureworks.silver TO "Project RL - Data Engineers"
+GRANT CREATE ON SCHEMA adventureworks.silver TO "Project RL - Data Engineers"
 
-GRANT USE SCHEMA ON SCHEMA gold.adventureworks TO "Developers"
-GRANT USE SCHEMA ON SCHEMA gold.adventureworks TO "Project RL - Data Engineers"
-GRANT SELECT ON TABLE bronze.adventureworks.products TO "Developers"
-GRANT SELECT ON TABLE bronze.adventureworks.products TO "Project RL - Data Engineers"GRANT CREATE ON SCHEMA gold.adventureworks TO "Project RL - Data Engineers"
+GRANT USE SCHEMA ON SCHEMA adventureworks.gold TO "Developers"
+GRANT USE SCHEMA ON SCHEMA adventureworks.gold TO "Project RL - Data Engineers"
+GRANT CREATE ON SCHEMA adventureworks.gold TO "Project RL - Data Engineers"
 
 -- COMMAND ----------
 
-GRANT SELECT ON TABLE bronze.adventureworks.products TO "Developers"
-GRANT SELECT ON TABLE bronze.adventureworks.products TO "Project RL - Data Engineers"
+GRANT SELECT ON TABLE adventureworks.bronze.products TO "Developers"
+GRANT SELECT ON TABLE adventureworks.bronze.products TO "Project RL - Data Engineers"
 
-GRANT SELECT ON TABLE bronze.adventureworks.sales_order_header TO "Developers"
-GRANT SELECT ON TABLE bronze.adventureworks.sales_order_header TO "Project RL - Data Engineers"
+GRANT SELECT ON TABLE adventureworks.bronze.sales_order_header TO "Developers"
+GRANT SELECT ON TABLE adventureworks.bronze.sales_order_header TO "Project RL - Data Engineers"
 
-GRANT SELECT ON TABLE bronze.adventureworks.sales_order_detail TO "Developers"
-GRANT SELECT ON TABLE bronze.adventureworks.sales_order_detail TO "Project RL - Data Engineers"
+GRANT SELECT ON TABLE adventureworks.bronze.sales_order_detail TO "Developers"
+GRANT SELECT ON TABLE adventureworks.bronze.sales_order_detail TO "Project RL - Data Engineers"
 
-GRANT SELECT ON TABLE silver.adventureworks.products TO "Developers"
-GRANT SELECT ON TABLE silver.adventureworks.products TO "Project RL - Data Engineers"
+GRANT SELECT ON TABLE adventureworks.silver.products TO "Developers"
+GRANT SELECT ON TABLE adventureworks.silver.products TO "Project RL - Data Engineers"
 
-GRANT SELECT ON TABLE silver.adventureworks.sales_order_header TO "Developers"
-GRANT SELECT ON TABLE silver.adventureworks.sales_order_header TO "Project RL - Data Engineers"
+GRANT SELECT ON TABLE adventureworks.silver.sales_order_header TO "Developers"
+GRANT SELECT ON TABLE adventureworks.silver.sales_order_header TO "Project RL - Data Engineers"
 
-GRANT SELECT ON TABLE silver.adventureworks.sales_order_detail TO "Developers"
-GRANT SELECT ON TABLE silver.adventureworks.sales_order_detail TO "Project RL - Data Engineers"
+GRANT SELECT ON TABLE adventureworks.silver.sales_order_detail TO "Developers"
+GRANT SELECT ON TABLE adventureworks.silver.sales_order_detail TO "Project RL - Data Engineers"
 
-GRANT SELECT ON TABLE gold.adventureworks.sales TO "Developers"
-GRANT SELECT ON TABLE gold.adventureworks.sales TO "Project RL - Data Engineers"
+GRANT SELECT ON TABLE adventureworks.gold.sales TO "Developers"
+GRANT SELECT ON TABLE adventureworks.gold.sales TO "Project RL - Data Engineers"
